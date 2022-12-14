6c5fb5af159368714e7077fb4608c6d080a5a2b0
darinatsybenko <110915599+darinatsybenko@users.noreply.github.com>
2022-12-14 19:26:28 +0300

    js
    commit

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -7,11 +7,7 @@ event.preventDefault();
 const {elements: {email, password}} = event.currentTarget;
 
 if(email.value === '' || password.value === '') {
-return alert('всі поля повинні бути заповнені')
+ alert('всі поля повинні бути заповнені')
 }
-const value ={
-    email: email.value,
-    password: password.value,
-}
-return formEl.reset();
+ formEl.reset();
 }
